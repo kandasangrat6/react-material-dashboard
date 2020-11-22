069322297248b72a7b8ffd5a40fc8969a808fd33
kandasangrat6 <kandasangrat6@gmail.com>
2020-11-22 10:51:01 +0700

    deploy build

---
+++ build/precache-manifest.cfbc9a4292b37a2d74e9384c84bb79f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6eb74967653d18d4c46ba9b9daa4786",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6430fe55ca2402146a8d",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "6430fe55ca2402146a8d",
+    "url": "/static/js/2.ad09dd94.chunk.js"
+  },
+  {
+    "revision": "6553848ede33e774934d8afdf700acf9",
+    "url": "/static/js/2.ad09dd94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2c25325984a7756789e",
+    "url": "/static/js/main.bac13445.chunk.js"
+  },
+  {
+    "revision": "96c7fa5d65fcbeec2ae4",
+    "url": "/static/js/runtime-main.9068f4d7.js"
+  }
+]);
